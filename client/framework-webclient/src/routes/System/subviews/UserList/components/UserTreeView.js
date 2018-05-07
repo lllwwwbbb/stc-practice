a90480db3cb2f8bbf83b186885ed6cc5e0dabecb
Jie Yang <jieyang@JiedeMacBook-Pro.local>
2018-05-07 22:00:37 +0800

    完成了前端与后台交互

--- client/framework-webclient/src/routes/System/subviews/UserList/components/UserTreeView.js
+++ client/framework-webclient/src/routes/System/subviews/UserList/components/UserTreeView.js
@@ -1,5 +1,7 @@
 import React, {Component, PropTypes} from 'react';
 
+import {getStore} from 'store/globalStore';
+
 import {Row, Col, Card, Tabs, Select, Button, Layout, Form, Input} from 'antd';
 const { Header, Sider, Content } = Layout;
 const FormItem = Form.Item;
@@ -16,8 +18,16 @@ export default class UserTreeView extends Component
 
 		this.state = 
 		{
-			organizationId: ''
+			organizationId: '',
+			name: 'name',
+			ID: 'ID'
 		}
+
+		this.getData=this.getData.bind(this);
+		this.setData=this.setData.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+
 	}
 
 	setOrganizationId = (organizationId) =>
@@ -28,25 +38,138 @@ export default class UserTreeView extends Component
 		});
 	}
 
-	/*static defaultProps = {
-		company:'贪玩蓝月',
-		telephone:'110',
-		weituo:1
-	}*/
+	handleChange(e) {
+		this.setState({name: e.target.value});
+	  }
+	
+
+	getData(){
+		return fetch('http://127.0.0.1:8000/entrust', 
+			{
+				method: "GET"
+			})
+			.then((res) => 
+			{
+				if(res.ok)
+				{
+					return res.json();
+				}
+				else
+				{
+					return Promise.reject();
+				}
+			})
+			.then(res=>
+			{
+				this.setState({name:res.entrustString})
+				this.setState({ID:res.id})
+				// debugger
+			})
+			
+	}
+
+	handleClick(){
+		let obj = {
+			entrustString: this.state.name,
+			id: "1"
+		};
+
+		// debugger
+  
+		fetch('http://127.0.0.1:8000/entrust', 
+		{  
+ 			method: 'PUT',  
+ 			headers: {},  
+			 // body: formData, 
+			body: JSON.stringify(obj) 
+			
+
+		})
+
+		.then((response) => 
+		{  
+			//debugger
+ 			if (response.ok) {  
+     			//return response.json();  
+ 			}  
+		})
+		.then((json) => 
+		{  
+ 			// alert(JSON.stringify(json));  
+		}).catch((error) => 
+		{  
+ 			console.error(error);  
+		});  
+	}
+
+	setData(){
+		// let formData = new FormData();  
+		// formData.append("entrustString","Peppa pig");  
+		// formData.append("id","STC_00001");  
+
+		let obj = {
+			entrustString: "Peppa Pig",
+			id: "1"
+		};
+
+		// debugger
+  
+		fetch('http://127.0.0.1:8000/entrust', 
+		{  
+ 			method: 'PUT',  
+ 			headers: {},  
+			 // body: formData, 
+			body: JSON.stringify(obj) 
+			
+
+		})
+
+		.then((response) => 
+		{  
+			//debugger
+ 			if (response.ok) {  
+     			//return response.json();  
+ 			}  
+		})
+		.then((json) => 
+		{  
+ 			// alert(JSON.stringify(json));  
+		}).catch((error) => 
+		{  
+ 			console.error(error);  
+		});  
+			
+	}
+
+	componentDidMount()
+	{
+		// debugger
+		//this.setData();
+		var res = this.getData();
+		res.then((json) => {
+		});
+	}
+
+	// debugger
 
 	render(){
 			return(
+			
 				<Form layout='vertical'>
-					<Card title='委托信息'>
+					<Card title = "委托信息" >
 						<Row>
 						<Col span={12}>
-							<FormItem label="委托单位">
-								<Input  onChange={this.props.company} defaultValue="江西贪玩技术有限公司" />
+							<FormItem label="委托名称">
+								<Input
+									value={this.state.name}
+									onChange={this.handleChange}
+								/>
 							</FormItem>
+							
 						</Col>
 						<Col span={12}>
-							<FormItem label="联系电话">
-								<Input  onChange={this.props.telephone} defaultValue="40062-33555" />
+							<FormItem label="委托ID">
+								{this.state.ID}
 							</FormItem>
 						</Col>
 						</Row>
@@ -57,8 +180,8 @@ export default class UserTreeView extends Component
 						<div>
 							<Row gutter={16}>
 								<Col span={6}></Col>
-								<Col span={6} offset={2}><Button type="primary" disabled={isDisabled}>提交</Button></Col>
-								<Col span={6}><Button htmlType="deny" disabled={isDisabled}>保存</Button></Col>
+								<Col span={6} offset={2}><Button type="primary" disabled={isDisabled} onClick={this.handleClick}>提交</Button></Col>
+								<Col span={6}><Button htmlType="deny" disabled={isDisabled} onClick={this.handleClick}>保存</Button></Col>
 								<Col span={6}></Col>
 							</Row>
 						</div>
